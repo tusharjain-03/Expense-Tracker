44295a1cc9c59623c6044c7d591a3567a5c88ff9
Tushar <tusharjain3901@gmail.com>
2024-02-17 18:07:24 +0530

    Expense-Tracker

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const dbConnect = require('./dbConnect.js');
 const app = express();
 app.use(express.json());
-const path = require("path");
 const usersRoute = require("./routes/usersRoute");
 const transactionsRoute = require("./routes/transactionsRoute");
 const port = process.env.PORT || 5000;
@@ -11,12 +10,15 @@ const port = process.env.PORT || 5000;
 app.use("/api/users/", usersRoute);
 app.use("/api/transactions/", transactionsRoute);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use('/', express.static('client/build'));
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname,'client/build/index.html'));
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => {
